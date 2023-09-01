dc162907d6e92a24f61a771b82f290dba825c2b5
ibilawalkhan <kbilawal84437@gmail.com>
2023-09-01 22:59:53 +0500

    Completed

---
+++ Project09_CountdownTimer/index.js
@@ -0,0 +1,32 @@
+"use strict";
+function calculateTimeRemaining(targetDate) {
+    const currentDate = new Date();
+    const timeDifference = targetDate.getTime() - currentDate.getTime();
+    return timeDifference;
+}
+function formatTime(milliseconds) {
+    const seconds = Math.floor(milliseconds / 1000) % 60;
+    const minutes = Math.floor(milliseconds / (1000 * 60)) % 60;
+    const hours = Math.floor(milliseconds / (1000 * 60 * 60)) % 24;
+    const days = Math.floor(milliseconds / (1000 * 60 * 60 * 24));
+    return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+}
+function startCountdown(targetDate) {
+    const intervalId = setInterval(() => {
+        const timeRemaining = calculateTimeRemaining(targetDate);
+        if (timeRemaining > 0) {
+            const formattedTime = formatTime(timeRemaining);
+            console.clear();
+            console.log(`Countdown Timer: `);
+            console.log(`Time remaining: ${formattedTime}`);
+        }
+        else {
+            console.clear();
+            console.log(`Countdown Timer:`);
+            console.log(`Time's up`);
+            clearInterval(intervalId);
+        }
+    }, 1000);
+}
+const targetDate = new Date('2023-09-10T12:00:00');
+startCountdown(targetDate);
